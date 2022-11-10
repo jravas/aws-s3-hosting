c2e525db407309f4ef0395296cfbe9460759d41c
jravas <josip@prototyp.digital>
2022-11-10 17:09:59 +0100

    Routing rules removed

--- main.tf
+++ main.tf
@@ -21,13 +21,4 @@ resource "aws_s3_bucket_website_configuration" "example" {
   error_document {
     key = "error.html"
   }
-
-  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
-  }
 }
