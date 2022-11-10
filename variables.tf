cf0998333eb07f1c3e62ed897ba55aeabfe53ebe
jravas <josip@prototyp.digital>
2022-11-10 20:36:59 +0100

    comments removed

--- variables.tf
+++ variables.tf
@@ -16,15 +16,3 @@ variable "aws_access_secret" {
 variable "aws_region" {
   description = "AWS Region"
 }
-
-
-# variable "instance_type" {
-#   description = "Type of EC2 instance to provision"
-#   default     = "t2.micro"
-# }
-
-# variable "instance_name" {
-#   description = "EC2 instance name"
-#   default     = "Provisioned by Terraform"
-# }
-
