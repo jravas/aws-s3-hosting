567b4ff4527bd86e98b93fca6840a74bf07e955c
jravas <josip@prototyp.digital>
2022-11-11 20:00:32 +0100

    CloudFront added

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   cloud {
-    organization = "your-organization-name"
+    organization = "prototyp"
     workspaces {
       name = "aws-s3-hosting"
     }
@@ -26,5 +26,47 @@ resource "aws_s3_bucket" "this" {
 
   tags = {
     Name = "AWS S3 Hosting"
+  }
+}
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "AWS S3 Hosting"
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.this.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
